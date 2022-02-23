6a1abcc5fc469f78b84496d8644e6d4dd0ffa7c0
2ntro <wodnjs6643@gmail.com>
2022-02-23 20:30:17 +0900

    fix: Drawer App Registry Bug

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
 import MainScreen from './screens/MainScreen';
 import DetailScreen from './screens/DetailScreen';
+import InfoScreen from './screens/InfoScreen';
 
 const Stack = createNativeStackNavigator();
-
+const Drawer = createDrawerNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Detail" component={DetailScreen} />
+        <Drawer.Screen name="Info" component={InfoScreen} />
+      </Drawer.Navigator>
+
+      {/* <Stack.Navigator>
         <Stack.Screen
           name="Main"
           component={MainScreen}
           options={{headerShown: false}}
         />
         <Stack.Screen name="Detail" component={DetailScreen} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
     </NavigationContainer>
   );
 }

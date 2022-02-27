01cbdbfe4d30d79d0042d14be95380472e76ada7
hoho00 <jhoya963@gmail.com>
2022-02-27 17:01:49 +0900

    feat: make member getter in HomeScreen

--- src/components/homeScreen/HomeScreenTitle.js
+++ src/components/homeScreen/HomeScreenTitle.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
-const HomeScreenTitle = ({style}) => {
-  return <Text>home screen</Text>;
+const HomeScreenTitle = ({memberData}) => {
+  return (
+    <>
+      <Text>home screen</Text>
+      <Text>{memberData.email}</Text>
+      <Text>{memberData.locationName}</Text>
+      <Text>{memberData.locationX}</Text>
+      <Text>{memberData.locationY}</Text>
+      <Text>{memberData.memberId}</Text>
+      <Text>{memberData.memberName}</Text>
+      <Text>{memberData.phoneNumber}</Text>
+    </>
+  );
 };
 
 export default HomeScreenTitle;
